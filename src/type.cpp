396345b68c1b09e65c573ecea8fb784084b4a6f2
wasabi-neko <wasabi.neko.owo@gmail.com>
2022-01-07 07:48:57 +0800

    Implement part of feature of type.hh

--- src/type.cpp
+++ src/type.cpp
@@ -3,3 +3,15 @@
 // -----------------------------------------------------------------------------
 // Variable type
 // -----------------------------------------------------------------------------
+
+void set_var_val(var_t *var, enum var_types type, int64_t val) {
+    var->_content = val |  (type_mask)type;
+    return;
+}
+
+bool type_check(const var_t var, enum var_types type) {
+    // return true if the type tag of the var is equal to the given type
+    return !((var._content & tag_mask) ^ (type_mask)type);
+}
+
+enum var_types get_type(const var_t);
--- src/type.hh
+++ src/type.hh
@@ -20,19 +20,30 @@ typedef void (*native_func_body_t)(func_t *self, env_t *env);
  * |01...| 32 bit int |
  * |11...| 48 bit ptr |
  */
+typedef enum TYPE_MASK {
+    tag_mask   = 0xff00000000000000,
+    bool_mask  = 0x4000000000000000,
+    int32_mask = 0x8000000000000000,
+    ptr48_mask = 0x0000000000000000,
+} type_mask;
+
 enum var_types {
-    list_bool, lisp_int32, list_ptr,
+    list_bool = bool_mask,
+    lisp_int32 = int32_mask,
+    list_ptr = ptr48_mask,
 };
 typedef union LISP_VAR_TYPE{
+    int64_t _content;
     bool lisp_bool;
     int lisp_int32;
     void *lisp_ptr;
 } var_t;
 
-void set_var_val(var_t *, enum var_types);
-enum var_types get_type(const var_t);
-bool type_check(const var_t);
-int get_int_val(const var_t);
+void set_var_val(var_t *, enum var_types, int64_t val);
+enum var_types get_type(const var_t, enum var_types);
+bool type_check(const var_t, enum var_types);
+
+int get_int_val(const var_t);       // ? maybe i don't need this 
 bool get_bool_val(const var_t);
 func_t *get_func_ptr(const var_t);
 
