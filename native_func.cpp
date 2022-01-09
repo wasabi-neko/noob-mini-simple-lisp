3277f4b0667705eefcf79344689a4fd32e4a5b5f
wasabi-neko <wasabi.neko.owo@gmail.com>
2022-01-09 23:41:36 +0800

    Add define operator body macro

--- native_func.cpp
+++ native_func.cpp
@@ -14,10 +14,37 @@ void LISP_NATIVE_FUNC_BODY_MAIN(func_t *self, env_t *env) {
     return;
 }
 
-void LISP_NATIVE_FUNC_BODY_ADD(func_t *self, env_t *env) {
-    env->result = set_var_val(lisp_int32, {.lisp_int32 = 48763});
-    return;
-}
+#define DEFINE_SINGLE_OPERATION_FUNC_BODY(name, type, lambda)                         \
+void LISP_NATIVE_FUNC_BODY##name (func_t *self, env_t *env) {                         \
+    int argc_given = env->data_stack.rsp - env->data_stack.rbp - 1;                   \
+    if (self->argc != -1 && argc_given != self->argc) {                               \
+        /*// ! error*/                                                                \
+        printf("%d argument need, but %d given\n", self->argc, argc_given);           \
+        exit(-1);                                                                     \
+        return;                                                                       \
+    }                                                                                 \
+    /* set first arg */                                                               \
+    var_t result = env->data_stack.stack[env->data_stack.rbp + 2];                    \
+    /* for every arg */                                                               \
+    for (int i = env->data_stack.rbp + 3; i <= env->data_stack.rsp; i++) {            \
+        var_t arg = env->data_stack.stack[i];                                         \
+        if (type_check(arg, type)) {                                                  \
+            lambda                                                                    \
+        } else {                                                                      \
+            /*// ! error*/                                                            \
+            printf("error: type error\n");                                            \
+            print_var_val(env->data_stack.stack[i]);                                  \
+            fflush(stdout);                                                           \
+            exit(-1);                                                                 \
+            return;                                                                   \
+        }                                                                             \
+    }                                                                                 \
+    env->result = set_var_val(type, {.type = result.type});                           \
+    return;                                                                           \
+}                                                                                     \
+ 
+DEFINE_SINGLE_OPERATION_FUNC_BODY(_ADD, lisp_int32, {result.lisp_int32 += arg.lisp_int32;});
+
 
 void LISP_NATIVE_FUNC_BODY_IF(func_t *self, env_t *env) {
     return;
