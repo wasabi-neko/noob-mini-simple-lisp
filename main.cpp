4232f143ce79adaeb1304d7ef37bb07649f58b65
wasabi-neko <wasabi.neko.owo@gmail.com>
2022-01-09 09:21:30 +0800

    Concept: write down some new concept of interpreter
    Note: I give up iterative AST interpreteing. Just use some simple
    recursive visiting approach to interprete the AST.
    By the way with this method, i need to chnage my grammar and AST a
    little bit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ extern "C" int yyparse(void);
 extern func_t lisp_main;
 
 void AST_interpreter(func_t *main, env_t *env);
+var_t interpret(AST_node*node, env_t *env);
 
 int main(int argc, char *argv[]) {
     yyparse();
@@ -18,51 +19,48 @@ void AST_interpreter(func_t *main, env_t *env) {
     AST_node *rip;
     // env->push_func(main);
 
-    //?  maybe i can just use longjump to escape this while loop
-    while (true) {  // while (!env->func_stack.empty())
-        while (rip != NULL) {
+    return;
+}
 
-            if (true) {     //* !env->top_func->allow_ast_param
-                if (rip->child != NULL) {
-                    // this node should be call-able 
-                    // if (get_func_ptr(rip->val) == NULL) {
-                    //     // type check is insdie of get_val function
-                    //     // !error
-                    // }
+var_t interpret(AST_node *root, env_t *env, bool allow_exp_arg) {
+    int next_func_rbp = rsp;
 
-                    // env->push_func(get_func_ptr(rip->val))
-                    // push rip
-                    // push rbp
-                    // next_rbp = rsp
-                    rip = rip->child;   // prepare child(arguments) to call the function
-                }
+
+    // push all list into stack
+    AST_node *node = root;
+    while (node != NULL) {
+        var_t val;
+        if (node->child != NULL) {      // if the node is a compound expression
+            if (allow_exp_arg) {
+                val = node;
+            } else {
+                env->add_frame();
+                val = interpret(node->child, env, node->child->property);
             }
-            // env->data_stack.push(rip->val);
-            rip = rip->next;
+        } else if (node->name != NULL) {
+            val = ask_val(env->top_func, name);
+        } else {
+            val = node->val;
         }
 
-        // func_top = env->func_stack.top
-        // if (func_top->type == release ) {        //* maybe i can just use NULL to prepresent return_function
-            // rsp = rbp
-            // rbp = stack.pop
-            // rip = stack.pop
-            // stack.push(env->result)
-        // } else {
-            // func_stack.pop();
-            // func_stack.push(return_func);
-            // rbp = next_rbp;
-            // if (func_top->is_native) {
-                // func_top->body->native_func(env);
-            // } else {
-                // if (func->have_scope) {
-                    // env->push_static_scope(rip->val);
-                // }
-                // rip = func_top->body->ast_body;
-            // }
-        // }
+        push_stack(val);
+        node = noed->next;
     }
-    
 
+    // function evoke
+    env->rbp = next_func_rbp;
+    func_t *func_to_call = env->call_local_var(0);
+    func_t *func_instance = create_runtime_func(func_to_call);
+    env->push_func(func_instance);  // push to stack and set static link and dynamic_link
 
-    return;
+    env->add_frame();
+    var_t result;
+    if (func_instance->is_native) {
+        result = func_instance->body.native_body(func_instance, env);   // natives should release frame inside
+    } else {
+        result = interpret(func_instance->body.ast_body, env, true);
+        env->release_frame();
+    }
+
+    return result;
 }
